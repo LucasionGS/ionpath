4ff8591085016c03ff9380c5d4257834d8b6ac17
Lucasion <lucasion@hotmail.com>
2019-12-10 20:04:56 +0100

    Added Node Module

---
+++ path.js
@@ -0,0 +1,28 @@
+exports.Path = class Path
+{
+  /**
+   * Converts a relative path into a full path.
+   * @param {string} relativePath Path to convert.
+   */
+  static relativeToFull(relativePath) {
+    relativePath = relativePath.replace(/\\+|\/\/+/g, "/");
+    if (relativePath.startsWith("./")) {
+      relativePath = relativePath.substring(2);
+    }
+    relativePath = Path.correct(__dirname+"/"+relativePath);
+    return relativePath;
+  }
+
+  /**
+   * Corrects a path's ``\`` into ``/`` and double slashes will turn into singles. Removes irrelevant ``./``.
+   * @param {string} path Path to correct
+   */
+  static correct(path) {
+    path = path.replace(/\\+|\/\/+/g, "/");
+    while (/\/\.\//g.test(path)) {
+      path = path.replace(/\/\.\//g, "/");
+    }
+
+    return path;
+  }
+}
